4b8cd6f54c8fd8f0b15062aa7699a59b147ef5ab
Angel <marcobluekpop@gmail.com>
2024-02-09 03:00:50 -0600

    Commit de Cambios en Servicios

---
+++ src/db/models/services.model.js
@@ -0,0 +1,46 @@
+import { Model, DataTypes, Sequelize } from 'sequelize';
+
+const SERVICE_TABLE = 'servicio';
+
+class Service extends Model {
+    static config(sequelize) {
+        return {
+            sequelize,
+            tableName: SERVICE_TABLE,
+            modelName: 'Servicio',
+            timestamps: false
+        }
+    }
+} 
+
+const ServiceSchema = {
+    id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+        field:'id_servicio'
+    },
+    name: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        field:'Nombre'
+    },
+    description: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        field:'Descripcion'
+    },
+    price: {
+        allowNull: false,
+        type: DataTypes.FLOAT,
+        field:'Precio'
+    },
+    image:{ 
+        allowNull:true,
+        type: DataTypes.STRING,
+        field: 'Imagen'
+    }
+}
+  
+export { Service, ServiceSchema };
