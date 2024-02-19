256f8eeb3ce57807c89973c379aee44bf8f0042c
arthurR3 <128752398+arthurR3@users.noreply.github.com>
2024-02-19 17:14:11 -0600

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-  env: process.env.NODE_ENV || 'dev',
   port: process.env.PORT || 3000,
   dbUser:  process.env.DB_USER,
   dbPassword:  process.env.DB_PASSWORD,
