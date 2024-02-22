1310623cbbb6ea1c1130787c342d41c539c11b5d
arthurR3 <128752398+arthurR3@users.noreply.github.com>
2024-02-22 01:11:13 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -19,7 +19,14 @@ app.use(express.json());
 app.get('/', (req,res) => {
     res.send('Backend con NodeJS - Express + CRUD API REST + MySQL');
 });
-
+app.get('/test-db', async (req, res) => {
+    try {
+        const result = await sequelize.query('SELECT 1+1 as result');
+        res.json({ result: result[0][0].result });
+    } catch (error) {
+        res.status(500).json({ error: 'Error al conectar con la base de datos' });
+    }
+});
 routerApi(app);
 
 app.listen(port,()=>{
