ff85a00000ee73740673abd7b5a491d457424999
arthurR3 <128752398+arthurR3@users.noreply.github.com>
2024-02-23 12:55:04 -0600

    Update sequelize.js

--- src/libs/sequelize.js
+++ src/libs/sequelize.js
@@ -3,11 +3,12 @@ import { Sequelize } from 'sequelize';
 import { config } from '../config/config.js';
 import setupModels from './../db/models/index.js';
 import mysql2 from 'mysql2'
-const sequelize = new Sequelize(
+const url = process.env.MYSQL_URL || 'mysql://root:dd1BA4C6-A456C6afaef234EfbHAD345@mysql.railway.internal:3306/railway';
+
+const sequelize = new Sequelize(url,
     config.dbName, // name database
     config.dbUser, // user database
     config.dbPassword, // password database
-    process.env.MYSQL_URL,
     {
         host: config.dbHost,
         dialect: 'mysql',
