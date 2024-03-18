c4b4563b6326a7d2d1053b66801546b3561a16b2
Angel <marcobluekpop@gmail.com>
2024-03-18 03:58:27 -0600

    Rutas agregadas de Categoria, Marca y Proveedor, consulta conjunta entre estas tablas y Producto

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,9 @@ import usersRouter from './users.router.js';
 import productsRouter from './products.router.js'
 import datesRouter from './dates.router.js'
 import servicesRouter from './services.router.js'
+import categoriesRouter from './categories.router.js'
+import brandsRouter from './brands.router.js'
+import suppliersRouter from './suppliers.router.js'
 
 function routerApi(app) {
   const router = Router();
@@ -12,6 +15,9 @@ function routerApi(app) {
   router.use('/products', productsRouter);
   router.use('/dates', datesRouter);
   router.use('/services', servicesRouter);
+  router.use('/categories', categoriesRouter);
+  router.use('/brands', brandsRouter);
+  router.use('/suppliers', suppliersRouter);
 }
 
 export default routerApi;
