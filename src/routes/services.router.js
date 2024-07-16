d8d0d183859e0cb0ec0740da60ddfd9790c8db73
arthurR3 <hedzjose014@gmail.com>
2024-07-15 22:33:28 -0600

    Update Cloudinary

--- src/routes/services.router.js
+++ src/routes/services.router.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
-const router = Router(); 
 import { get, getById, create, update, _delete } from '../controllers/services.controller.js';
+import upload from '../config/multerConfig.js';
+const router = Router(); 
 
 router
     .get('/', get )
     .get('/:id', getById )
-    .post('/', create )
+    .post('/', upload, create )
     .put('/:id', update )
     .delete('/:id', _delete );
 
