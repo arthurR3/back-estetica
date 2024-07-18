0930a33e9aec49c0f6899d70217aa94d35946e4d
arthurR3 <hedzjose014@gmail.com>
2024-07-17 22:16:15 -0600

    Update req.body.image

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 const router = Router(); 
 import { get, getById, create, update, _delete } from '../controllers/products.controller.js';
-
+import upload from '../config/multerConfig.js'
 router
     .get('/', get )
     .get('/:id', getById )
-    .post('/', create )
-    .put('/:id', update )
+    .post('/',upload, create )
+    .put('/:id', upload, update )
     .delete('/:id', _delete );
 
 export default router;
