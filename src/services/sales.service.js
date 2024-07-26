f3e0b058ad37ce7251192a4197f57236865da736
arthurR3 <hedzjose014@gmail.com>
2024-07-26 06:44:43 -0600

    modified:   src/controllers/products.controller.js     modified:   src/controllers/sales.controller.js     modified:   src/controllers/services.controller.js 	modified:   src/services/sales.service.js
    	modified:   src/controllers/sales.controller.js

--- src/services/sales.service.js
+++ src/services/sales.service.js
@@ -50,6 +50,47 @@ class SalesService {
         const res = await Sale.findByPk(id);
         return res;
     }
+    async findByUserId(userId) {
+        try {
+            const sales = await Sale.findAll({
+                where: {
+                    id_user: userId
+                },
+                include: [
+                    {
+                        model: User, // Información del usuario
+                        attributes: ['id', 'name', 'last_name1', 'last_name2', 'phone', 'email'] // Atributos del usuario
+                    },
+                    {
+                        model: SaleDetail, // Detalles de la venta
+                        attributes: ['id', 'amount', 'unit_price', 'subtotal'], // Atributos del detalle
+                        include: [
+                            {
+                                model: Product, // Información del producto
+                                attributes: ['id', 'id_category', 'id_brand', 'name', 'description', 'price'], // Atributos del producto
+                                include: [
+                                    {
+                                        model: Category, // Información de la categoría
+                                        attributes: ['id', 'name'] // Atributos de la categoría
+                                    },
+                                    {
+                                        model: Brand, // Información de la marca
+                                        attributes: ['id', 'name'] // Atributos de la marca
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            });
+    
+            return sales || []; // Devuelve el resultado o un array vacío si no hay resultados
+        } catch (error) {
+            console.error('Error al obtener las ventas por ID de usuario:', error);
+            throw error;
+        }
+    }
+    
 
     async create(data) {
         const res = await Sale.create(data);
